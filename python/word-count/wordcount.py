4f4a84bfac1f45bfd05121cb0aee495d3253c7e2
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-13 17:43:34 +0200

    Use collections.Counter

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,4 +1,5 @@
 import re
+import collections
 
 class Phrase:
 
@@ -8,27 +9,10 @@ class Phrase:
     self.text = text
 
   def word_count(self):
-    return OccurrenceCounter().count_all(self._normalized_words()).dict()
+    return dict(collections.Counter(self._normalized_words()))
 
   def _normalized_words(self):
     return Phrase.WORDS.findall(self._normalized_text())
 
   def _normalized_text(self):
     return self.text.lower()
-
-class OccurrenceCounter:
-
-  def __init__(self):
-    self.counters = {}
-
-  def count_all(self, items):
-    for item in items:
-      self.count(item)
-    return self
-
-  def count(self, item):
-    self.counters[item] = self.counters.get(item, 0) + 1
-    return self
-
-  def dict(self):
-    return self.counters
