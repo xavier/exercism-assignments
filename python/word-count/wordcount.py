171ac5eba149f7a1041b01bde732a201a484fc1c
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-13 20:00:39 +0200

    Renamed regex constant

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -3,7 +3,7 @@ import collections
 
 class Phrase:
 
-  WORDS = re.compile('\w+')
+  WORDS_RE = re.compile('\w+')
 
   def __init__(self, text):
     self.text = text
@@ -12,7 +12,7 @@ class Phrase:
     return dict(collections.Counter(self._normalized_words()))
 
   def _normalized_words(self):
-    return Phrase.WORDS.findall(self._normalized_text())
+    return Phrase.WORDS_RE.findall(self._normalized_text())
 
   def _normalized_text(self):
     return self.text.lower()
