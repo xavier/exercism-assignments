249a580d908dbdadbe5c331b7ae757ce57b68b0e
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-13 20:01:42 +0200

    First iteration of nucleotide-count in Ruby

---
+++ ruby/nucleotide-count/dna.rb
@@ -0,0 +1,30 @@
+class DNA
+
+  COUNTABLE_NUCLEOTIDES = %w{A C G T U}
+
+  def initialize(sequence)
+    @sequence = sequence
+  end
+
+  def count(nucleotide)
+    raise ArgumentError unless COUNTABLE_NUCLEOTIDES.include?(nucleotide)
+    @sequence.count(nucleotide)
+  end
+
+  def nucleotide_counts
+    nucleotides.each_with_object(nucleotide_counters) do |nucleotide, counters|
+      counters[nucleotide] += 1
+    end
+  end
+
+  private
+
+  def nucleotides
+    @sequence.split('')
+  end
+
+  def nucleotide_counters
+    {'A' => 0, 'C' => 0, 'G' => 0, 'T' => 0}
+  end
+
+end
