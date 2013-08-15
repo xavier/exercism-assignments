a06a30f2000c09a4c3fd642eb2cc8b63f67f7a2f
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-16 00:14:48 +0200

    First iteration of anagram in Clojure

---
+++ clojure/anagram/anagram.clj
@@ -0,0 +1,13 @@
+(ns anagram)
+(require '[clojure.string :as string])
+
+(defn- comparable [word]
+  (sort (string/lower-case word)))
+
+(defn- anagram-matcher [word]
+  (let [comparable-word (comparable word)]
+    (fn [other-word]
+      (= comparable-word (comparable other-word)))))
+
+(defn anagrams-for [word candidates]
+  (filter (anagram-matcher word) candidates))
