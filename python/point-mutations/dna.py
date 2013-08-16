0ebdaa628fc3d5f54ccfa31ef1b9ecc81adb6cf7
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-16 21:56:42 +0200

    Refactored to use reduce with an accumulator

--- python/point-mutations/dna.py
+++ python/point-mutations/dna.py
@@ -1,12 +1,15 @@
-import itertools
+from itertools import izip
 
 class DNA:
   def __init__(self, sequence):
     self.sequence = sequence
 
   def hamming_distance(self, other_sequence):
-    d = 0
-    for a, b in itertools.izip(self.sequence, other_sequence):
-      if a != b:
-        d += 1
-    return d
+    return reduce(self._hamming_distance_accumulator, izip(self.sequence, other_sequence), 0)
+
+  def _hamming_distance_accumulator(self, distance, pair_of_nucleotides):
+    n1, n2 = pair_of_nucleotides
+    if n1 == n2:
+      return distance
+    else:
+      return distance + 1
