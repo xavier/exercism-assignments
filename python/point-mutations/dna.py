341cf469f1262ab1ef4b3a8e03eba99df6391345
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-16 21:48:03 +0200

    Use a generator for memory efficiency

--- python/point-mutations/dna.py
+++ python/point-mutations/dna.py
@@ -1,10 +1,12 @@
+import itertools
+
 class DNA:
   def __init__(self, sequence):
     self.sequence = sequence
 
   def hamming_distance(self, other_sequence):
     d = 0
-    for a, b in zip(self.sequence, other_sequence):
+    for a, b in itertools.izip(self.sequence, other_sequence):
       if a != b:
         d += 1
     return d
