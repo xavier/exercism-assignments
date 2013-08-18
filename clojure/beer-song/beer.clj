4a694cbcc5f5b71c994d8ad9c04d93f8fd90cf23
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-18 09:44:15 +0200

    Renamed line methods

--- clojure/beer-song/beer.clj
+++ clojure/beer-song/beer.clj
@@ -11,11 +11,11 @@
 (defn- take-what [count]
   (if (= count 1) "it" "one"))
 
-(defn- line1 [bottles]
+(defn- first-line [bottles]
   (let [formatted-bottles (n-bottles bottles)]
     (format "%s of beer on the wall, %s of beer." (string/capitalize formatted-bottles) formatted-bottles)))
 
-(defn- line2 [bottles]
+(defn- second-line [bottles]
   (if (> bottles 0)
     (format "Take %s down and pass it around, %s of beer on the wall." (take-what bottles) (n-bottles (dec bottles)))
     "Go to the store and buy some more, 99 bottles of beer on the wall."))
@@ -27,7 +27,7 @@
   (range bottles_available (dec bottles_left) -1))
 
 (defn verse [bottles]
-  (assemble [(line1 bottles) (line2 bottles)]))
+  (assemble [(first-line bottles) (second-line bottles)]))
 
 (defn sing
   ([bottles_available] (sing bottles_available 0))
