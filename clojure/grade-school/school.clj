0b266c06536b0c04e4c22557917a8104045d3018
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-21 22:56:22 +0200

    First iteration of Grade School in Clojure

---
+++ clojure/grade-school/school.clj
@@ -0,0 +1,12 @@
+(ns school)
+
+(def empty-grade [])
+
+(defn grade [db g]
+  (get db g empty-grade))
+
+(defn add [db student student-grade]
+  (assoc db student-grade (conj (grade db student-grade) student)))
+
+(defn sorted [db]
+  (into (sorted-map) (for [[g students] db] [g (sort students)])))
