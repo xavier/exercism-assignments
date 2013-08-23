5eaa2bb5d0c283365818b0583236d9565244e7d6
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-23 22:04:50 +0200

    One-liner to merged the dictionaries

--- python/nucleotide-count/dna.py
+++ python/nucleotide-count/dna.py
@@ -14,9 +14,7 @@ class DNA:
       raise ValueError, ("%s is not a nucleotide." % nucleotide)
 
   def nucleotide_counts(self):
-    counts = self._default_nucleotide_counts()
-    counts.update(self.counter)
-    return counts
+    return dict(self._default_nucleotide_counts() + self.counter.items())
 
   def _default_nucleotide_counts(self):
-    return {"A": 0, "C": 0, "G": 0, "T": 0}
+    return {"A": 0, "C": 0, "G": 0, "T": 0}.items()
