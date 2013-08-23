dff95059a7c2f5819a04aed1b7b146002707de2a
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-23 22:01:43 +0200

    Instantiate the Counter in the constructor

--- python/nucleotide-count/dna.py
+++ python/nucleotide-count/dna.py
@@ -5,21 +5,18 @@ class DNA:
   COUNTABLE_NUCLEOTIDES = "ACGTU"
 
   def __init__(self, sequence):
-    self.sequence = sequence
+    self.counter = Counter(sequence)
 
   def count(self, nucleotide):
     if nucleotide in DNA.COUNTABLE_NUCLEOTIDES:
-      return self._counter()[nucleotide]
+      return self.counter[nucleotide]
     else:
       raise ValueError, ("%s is not a nucleotide." % nucleotide)
 
   def nucleotide_counts(self):
     counts = self._default_nucleotide_counts()
-    counts.update(self._counter())
+    counts.update(self.counter)
     return counts
-
-  def _counter(self):
-    return Counter(self.sequence)
 
   def _default_nucleotide_counts(self):
     return {"A": 0, "C": 0, "G": 0, "T": 0}
