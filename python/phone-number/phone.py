bea5e6059edf99dcf028c10cd9143df4d58207df
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-27 19:28:35 +0200

    First iteration of Phone Number in Python

---
+++ python/phone-number/phone.py
@@ -0,0 +1,31 @@
+import re
+
+class Phone:
+
+  REGEX_SANITIZE = re.compile('[^\d]')
+  REGEX_STRIP_1  = re.compile('\A1(\d{10})\Z')
+  REGEX_PARTS    = re.compile('\A(\d{3})(\d{3})(\d{4})\Z')
+  INVALID        = ("000", "000", "0000")
+  PRETTY_FORMAT  = "(%s) %s-%s"
+
+  def __init__(self, number_string):
+    self.parts  = self._parse(self._clean(number_string))
+    self.number = "".join(self.parts)
+
+  def area_code(self):
+    return self.parts[0]
+
+  def pretty(self):
+    return Phone.PRETTY_FORMAT % self.parts
+
+  def _clean(self, number_string):
+    clean = Phone.REGEX_SANITIZE.sub("", number_string)
+    clean = Phone.REGEX_STRIP_1.sub("\\1", clean)
+    return clean
+
+  def _parse(self, clean_number_string):
+    match = Phone.REGEX_PARTS.search(clean_number_string)
+    if match:
+      return match.groups()
+    else:
+      return Phone.INVALID
