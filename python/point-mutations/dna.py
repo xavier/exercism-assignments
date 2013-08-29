bf71e03f86ad4ae76742bf15211beee02d28a86f
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-29 16:34:26 +0200

    Refactored to use list comprehension

--- python/point-mutations/dna.py
+++ python/point-mutations/dna.py
@@ -5,11 +5,4 @@ class DNA:
     self.sequence = sequence
 
   def hamming_distance(self, other_sequence):
-    return reduce(self._hamming_distance_accumulator, izip(self.sequence, other_sequence), 0)
-
-  def _hamming_distance_accumulator(self, distance, pair_of_nucleotides):
-    n1, n2 = pair_of_nucleotides
-    if n1 == n2:
-      return distance
-    else:
-      return distance + 1
+    return len([True for (n1, n2) in izip(self.sequence, other_sequence) if n1 != n2])
