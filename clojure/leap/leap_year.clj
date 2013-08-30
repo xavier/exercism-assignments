fe5fdcb1dff934018f0a2e5044f7652bdf6975ed
Xavier Defrang <xavier.defrang@gmail.com>
2013-08-30 23:36:14 +0200

    Leap year in Clojure

---
+++ clojure/leap/leap_year.clj
@@ -0,0 +1,15 @@
+;(ns leap_year)
+
+(defn- make-divisible-by [divisor]
+  #(= (mod %1 divisor) 0))
+
+(def divisible-by-4?   (make-divisible-by 4))
+(def divisible-by-100? (make-divisible-by 100))
+(def divisible-by-400? (make-divisible-by 400))
+
+(defn leap-year [year]
+  (cond
+    (divisible-by-400? year) true
+    (divisible-by-100? year) false
+    (divisible-by-4? year)   true
+    :else                    false))
