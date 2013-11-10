d614bac23b1589923bd884306daed797819aeff7
Xavier Defrang <xavier.defrang@gmail.com>
2013-11-10 10:13:36 +0100

    Hamming Distance in Ruby, aka Point Mutations revisited

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,55 @@
+# Already completed this assignment back when it was called 'Point Mutations'
+# Let's just delegate to my previous implementation
+module Hamming
+
+  def self.compute(sequence, other_sequence)
+    DNA.new(sequence).hamming_distance(other_sequence)
+  end
+
+end
+
+class DNA
+
+  def initialize(sequence)
+    @sequence = sequence
+  end
+
+  def hamming_distance(other_sequence)
+    nucleotide_sequence_pair(other_sequence).different.count
+  end
+
+  private
+
+  def nucleotide_sequence_pair(other_sequence)
+    NucleotidePairs.new(nucleotides(@sequence), nucleotides(other_sequence))
+  end
+
+  def nucleotides(seq)
+    seq.chars
+  end
+
+end
+
+class NucleotidePairs
+
+  def initialize(seq1, seq2)
+    @seq1, @seq2 = seq1, seq2
+  end
+
+  def different
+    comparable.select { |n1, n2| n1 != n2 }
+  end
+
+  def comparable
+    pairs.first(comparable_length)
+  end
+
+  def comparable_length
+    [@seq1.size, @seq2.size].min
+  end
+
+  def pairs
+    @seq1.zip(@seq2)
+  end
+
+end
