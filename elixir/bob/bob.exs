e3683651f686483dd695dcc3737419c96b9b343f
Xavier Defrang <xavier.defrang@gmail.com>
2014-05-06 17:39:07 +0200

    Refactored to use helper functions

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,14 +1,23 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-      String.strip(input) == "" ->
-        "Fine. Be that way!"
-      input == String.upcase(input) ->
-        "Woah, chill out!"
-      String.ends_with?(input, "?") ->
-        "Sure."
-      true ->
-        "Whatever."
+      silence?(input)  -> "Fine. Be that way!"
+      shouting?(input) -> "Woah, chill out!"
+      question?(input) -> "Sure."
+      true             -> "Whatever."
     end
   end
+
+  defp silence?(input) do
+    String.strip(input) == ""
+  end
+
+  defp shouting?(input) do
+    String.upcase(input) == input
+  end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
+  end
+
 end
