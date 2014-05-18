7615f5637a0eb4b24af90359efe927c57d5114fa
Xavier Defrang <xavier.defrang@gmail.com>
2014-05-18 19:11:23 +0200

    Reverse string to simplify the conversion process

--- ruby/trinary/trinary.rb
+++ ruby/trinary/trinary.rb
@@ -3,13 +3,12 @@ class Trinary
   POWERS_OF_3 = Hash.new { |cache, nth_power| cache[nth_power] = 3**nth_power }
 
   def initialize(string_of_digits)
-    @digits = string_of_digits.chars
+    @digits = string_of_digits.chars.reverse
   end
 
   def to_decimal
     (0...@digits.length).reduce(0) do |sum_of_powers, nth_digit|
-      position = @digits.length-(nth_digit+1)
-      sum_of_powers + (@digits[position].to_i * POWERS_OF_3[nth_digit])
+      sum_of_powers + (@digits[nth_digit].to_i * POWERS_OF_3[nth_digit])
     end
   end
 
