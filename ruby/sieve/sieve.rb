3a2de6ee0050cba297ece8cafb961eac629e0107
Xavier Defrang <xavier.defrang@gmail.com>
2014-05-19 19:51:49 +0200

    Square optimization

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -9,7 +9,7 @@ class Sieve
   def primes
     candidates = (2..@upper_bound).to_a
     n = 2
-    while n < candidates.last do
+    while (n*n) <= candidates.last do
       candidates.reject! { |c| (c != n) && (c % n).zero? }
       n += 1
     end
