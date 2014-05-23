47e34801413f3930cf0a3ad47582464dc57f67d8
Xavier Defrang <xavier.defrang@gmail.com>
2014-05-23 19:40:29 +0200

    Step 1 of Simple Cipher in Ruby (random key shift)

---
+++ ruby/simple-cipher/cipher.rb
@@ -0,0 +1,32 @@
+class Cipher
+
+  BASE = 'a'.ord
+
+  attr_reader :key
+
+  def initialize(key = nil)
+    @key ||= (BASE + Random.rand(26)).chr * 100
+  end
+
+  def encode(plain_text)
+    transform_with_key(plain_text) { |c, k| c + k }
+  end
+
+  def decode(obfuscated)
+    transform_with_key(obfuscated) { |c, k| c - k }
+  end
+
+  private
+
+
+  def bytes(string)
+    string.chars.map { |c| c.ord - BASE }
+  end
+
+  def transform_with_key(string, &block)
+    bytes(string).zip(bytes(key)).map do |c, k|
+      (BASE + block.(c, k)).chr
+    end.join
+  end
+
+end
