ee19861b4dbe5ee77ceaeda3dd7f7c64a9f87859
Xavier Defrang <xavier.defrang@gmail.com>
2014-05-23 20:12:49 +0200

    First iteration of Luhn check in Ruby

---
+++ ruby/luhn/luhn.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+
+class Luhn
+
+  def initialize(number)
+    @digits = number.to_s.scan(/\d/).map(&:to_i)
+  end
+
+  def addends
+    @digits.each_with_index.map { |d, i| multiply_digit(@digits.size-i, d)  }
+  end
+
+  def checksum
+    addends.reduce(:+)
+  end
+
+  def valid?
+    checksum % 10 == 0
+  end
+
+  def self.create(n)
+    luhn = Luhn.new(n*10)
+    n*10 + (10 - luhn.checksum) % 10
+  end
+
+  private
+
+  def multiply_digit(position, digit)
+    if position.even?
+      times2 = digit*2
+      times2 > 9 ? times2-9 : times2
+    else
+      digit
+    end
+  end
+
+end
