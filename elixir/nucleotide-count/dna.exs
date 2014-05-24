8ed8e9a8a59bf96b41a5437f12a38d554fbe21fa
Xavier Defrang <xavier.defrang@gmail.com>
2014-05-24 22:08:56 +0200

    Nucleotide count in Elixir

---
+++ elixir/nucleotide-count/dna.exs
@@ -0,0 +1,41 @@
+defmodule DNA do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  # @doc """
+  # Counts individual nucleotides in a DNA strand.
+
+  # ## Examples
+
+  # iex> DNA.count('AATAA', ?A)
+  # 4
+
+  # iex> DNA.count('AATAA', ?T)
+  # 1
+  # """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    strand |> Enum.count fn (n) -> n == nucleotide end
+  end
+
+
+  # @doc """
+  # Returns a summary of counts by nucleotide.
+
+  # ## Examples
+
+  # iex> DNA.nucleotide_counts('AATAA')
+  # Enum.into [{?A, 4}, {?T, 1}, {?C, 0}, {?G, 0}], HashDict.new
+  # """
+  @spec nucleotide_counts([char]) :: HashDict.t
+  def nucleotide_counts(strand) do
+    strand |> Enum.reduce(_nucleotide_counters, &_count_nucleotide/2)
+  end
+
+  defp _count_nucleotide(nucleotide, counters) do
+    Dict.update!(counters, nucleotide, &(&1 + 1))
+  end
+
+  defp _nucleotide_counters do
+    @nucleotides |> Enum.map(&({&1, 0})) |> Enum.into HashDict.new
+  end
+end
