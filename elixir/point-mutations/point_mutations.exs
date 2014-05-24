afa4b81d2c4341ca540e03558d8e02cfccf27576
Xavier Defrang <xavier.defrang@gmail.com>
2014-05-25 00:03:56 +0200

    Point mutations (Hamming distance) in ELixir

---
+++ elixir/point-mutations/point_mutations.exs
@@ -0,0 +1,18 @@
+defmodule DNA do
+  @doc """
+  Returns number of differences between two strands of DNA, known as the Hamming Distance.
+
+  ## Examples
+
+	iex> DNA.hamming_distance('AAGTCATA', 'TAGCGATC')
+	4
+	"""
+	@spec hamming_distance([char], [char]) :: non_neg_integer
+	def hamming_distance(strand1, strand2) do
+    Enum.zip(strand1, strand2) |> Enum.reduce(0, &_calculate_distance/2)
+	end
+
+  defp _calculate_distance({a,a}, distance), do: distance
+  defp _calculate_distance({a,b}, distance), do: distance + 1
+
+end
---
+++ elixir/point-mutations/point_mutations_test.exs
@@ -0,0 +1,40 @@
+if System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("example.exs")
+else
+  Code.load_file("point_mutations.exs")
+end
+
+ExUnit.start
+
+defmodule DNATest do
+  use ExUnit.Case, async: true
+  doctest DNA
+
+  test "no difference between empty strands" do
+    assert DNA.hamming_distance('', '') == 0
+  end
+
+  test "no difference between identical strands" do
+    assert DNA.hamming_distance('GGACTGA', 'GGACTGA') == 0
+  end
+
+  test "hamming distance in off by one strand" do
+    assert DNA.hamming_distance('GGACGGATTCTGACCTGGACTAATTTTGGGG', 'AGGACGGATTCTGACCTGGACTAATTTTGGGG') == 19
+  end
+
+  test "small hamming distance in middle somewhere" do
+    assert DNA.hamming_distance('GGACG', 'GGTCG') == 1
+  end
+
+  test "larger distance" do
+    assert DNA.hamming_distance('ACCAGGG', 'ACTATGG') == 2
+  end
+
+  test "ignores extra length on other strand when longer" do
+    assert DNA.hamming_distance('AAACTAGGGG', 'AGGCTAGCGGTAGGAC') == 3
+  end
+
+  test "ignores extra length on original strand when longer" do
+    assert DNA.hamming_distance('GACTACGGACAGGGTAGGGAAT', 'GACATCGCACACC') == 5
+  end
+end
