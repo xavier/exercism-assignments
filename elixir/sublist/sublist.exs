11f682d9c0e8b703728e0f94e2fdecf24ae7adb8
Xavier Defrang <xavier.defrang@gmail.com>
2014-06-06 11:09:28 +0200

    Removed leading underscore of private function names

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -7,24 +7,24 @@ defmodule Sublist do
   def compare(a, b) do
     cond do
       a === b           -> :equal
-      _sublist?(a, b)   -> :sublist
-      _superlist?(a, b) -> :superlist
+      sublist?(a, b)   -> :sublist
+      superlist?(a, b) -> :superlist
       true              -> :unequal
     end
   end
 
-  defp _sublist?([], _), do: true
-  defp _sublist?(_, []), do: false
-  defp _sublist?(a = [head_of_a|tail_of_a], [head_of_b|tail_of_b]) when head_of_a === head_of_b do
+  defp sublist?([], _), do: true
+  defp sublist?(_, []), do: false
+  defp sublist?(a = [head_of_a|tail_of_a], [head_of_b|tail_of_b]) when head_of_a === head_of_b do
     if tail_of_a === Enum.take(tail_of_b, Enum.count(a)-1) do
       true
     else
-      _sublist?(a, tail_of_b)
+      sublist?(a, tail_of_b)
     end
   end
-  defp _sublist?(a, [head_of_b|tail_of_b]), do: _sublist?(a, tail_of_b)
+  defp sublist?(a, [head_of_b|tail_of_b]), do: sublist?(a, tail_of_b)
 
-  def _superlist?(a, b), do: _sublist?(b, a)
+  def superlist?(a, b), do: sublist?(b, a)
 
 end
 
