5c0595693d52811ba21ef16244b80554aa1805ec
Xavier Defrang <xavier.defrang@gmail.com>
2014-06-08 10:54:51 +0200

    Binary Search Tree in Ruby

---
+++ ruby/binary-search-tree/bst.rb
@@ -0,0 +1,35 @@
+
+class Bst
+
+  attr_reader :data, :left, :right
+
+  def initialize(data)
+    @data = data
+  end
+
+  def insert(new_data)
+    if new_data <= @data
+      @left  = insert_branch(@left, new_data)
+    else
+      @right = insert_branch(@right, new_data)
+    end
+  end
+
+  def each(&block)
+    left.each(&block) if left
+    yield data
+    right.each(&block) if right
+  end
+
+  private
+
+  def insert_branch(branch, new_data)
+    if branch
+      branch.insert(new_data)
+      branch
+    else
+      Bst.new(new_data)
+    end
+  end
+
+end
