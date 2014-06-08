e42598ef68f68308974946f4b675152101dc3469
Xavier Defrang <xavier.defrang@gmail.com>
2014-06-08 10:06:21 +0200

    Refactored to use reduce and an internal exception to bail out on invalid input

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -1,22 +1,24 @@
 
 class Binary
 
-  def initialize(bits)
-    @bits = bits
+  BIT_0 = "0"
+  BIT_1 = "1"
+
+  class InvalidDigit < StandardError ; end
+
+  def initialize(string_of_bits)
+    @bits = string_of_bits.chars
   end
 
   def to_decimal
-    decimal_value = 0
-    @bits.chars.each_with_index do |bit, position|
-      case bit
-      when "1"
-        decimal_value += 1 << (@bits.size-position-1)
-      when "0"
-      else
-        return 0
-      end
+    @bits.reduce(0) do |decimal_value, bit|
+      decimal_value <<= 1
+      if    bit == BIT_1 then decimal_value += 1
+      elsif bit != BIT_0 then raise InvalidDigit end
+      decimal_value
     end
-    decimal_value
+  rescue InvalidDigit
+    0
   end
 
 end
