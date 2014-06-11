2adf5434768e46532bee23e4d84d3730a1142f4f
Xavier Defrang <xavier.defrang@gmail.com>
2014-06-11 10:04:16 +0200

    Fixed indentation

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -4,13 +4,13 @@ defmodule DNA do
 
   ## Examples
 
-	iex> DNA.hamming_distance('AAGTCATA', 'TAGCGATC')
-	4
-	"""
-	@spec hamming_distance([char], [char]) :: non_neg_integer
-	def hamming_distance(strand1, strand2) do
+  iex> DNA.hamming_distance('AAGTCATA', 'TAGCGATC')
+  4
+  """
+  @spec hamming_distance([char], [char]) :: non_neg_integer
+  def hamming_distance(strand1, strand2) do
     Enum.zip(strand1, strand2) |> Enum.reduce(0, &_calculate_distance/2)
-	end
+  end
 
   defp _calculate_distance({_a,_a}, distance), do: distance
   defp _calculate_distance({_a,_b}, distance), do: distance + 1
