b3317ec02f192219dc2a029fddcba379cd701266
Xavier Defrang <xavier.defrang@gmail.com>
2014-07-01 16:04:38 +0200

    Use a more generic function to removed special case for earth

--- clojure/space-age/space_age.clj
+++ clojure/space-age/space_age.clj
@@ -3,6 +3,7 @@
 (def ^:const earth-orbital-period-in-seconds 31557600.0)
 
 (def ^:const orbital-periods-relative-to-earth {
+  "earth"   1
   "mercury" 0.2408467
   "venus"   0.61519726
   "mars"    1.8808158
@@ -12,10 +13,10 @@
   "neptune" 164.79132
 })
 
-(defn on-earth [age-in-seconds] (/ age-in-seconds earth-orbital-period-in-seconds))
-
 (defn- age-on-planet-converter [planet]
-  (fn [age-in-seconds] (/ (on-earth age-in-seconds) (orbital-periods-relative-to-earth planet))))
+ (fn [age-in-seconds]
+    (let [age-on-earth (/ age-in-seconds earth-orbital-period-in-seconds)]
+      (/ age-on-earth (orbital-periods-relative-to-earth planet)))))
 
 (defn- define-on-planet-functions []
   (doseq
