38c3f5fcbf117a6e7672afda3f7031f1514d3c2b
Xavier Defrang <xavier.defrang@gmail.com>
2014-07-03 22:44:02 +0200

    Simplified Hamming distance calculation using Stream.zip and Enum.count

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -9,10 +9,7 @@ defmodule DNA do
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
   def hamming_distance(strand1, strand2) do
-    Enum.zip(strand1, strand2) |> Enum.reduce(0, &calculate_distance/2)
+    Stream.zip(strand1, strand2) |> Enum.count(fn ({a, b}) -> a != b end)
   end
-
-  defp calculate_distance({_a,_a}, distance), do: distance
-  defp calculate_distance({_a,_b}, distance), do: distance + 1
 
 end
